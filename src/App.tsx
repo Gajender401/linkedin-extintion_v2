48f927e61a2d40d61aa72ab118fef8f28df23549
Gajender401 <kgajender86@gmail.com>
2024-02-09 23:13:18 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
-import Template from './pages/template';
-import Templates from './pages/templates';
 import { useUserAuth } from './context/context';
 import Login from './pages/login';
 import { useEffect, useRef, useState } from 'react';
-import Message from './components/message';
+// import Message from './components/message';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { Check, ChevronsUpDown } from "lucide-react"
 
+import { cn } from "./@/lib/utils"
+import { Button } from "./@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "./@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "./@/components/ui/popover"
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+]
+
+interface Template {
+  id: string;
+  name: string;
+  content: string;
+  user: string;
+}
 
 const Popup = () => {
 
@@ -17,6 +60,34 @@ const Popup = () => {
   const [position, setPosition] = useState({ x: 20, y: 20 });
   const initialMousePosition = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
   const divRef = useRef<HTMLDivElement>(null);
+  const [templates, setTemplates] = useState<Template[]>([])
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState("")
+
+  async function getTemplates(tokken: string) {
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://bot.kaliper.in/api/api/templates/',
+      headers: {
+        'Authorization': `Bearer ${tokken}`
+      }
+    };
+
+    await axios.request(config)
+      .then((response: any) => {
+        setTemplates(response.data)
+        console.log(templates,response.data);
+      })
+
+  }
+
+
+  useEffect(() => {
+    if (access) {
+      getTemplates(access)
+    }
+  }, [access])
 
 
   const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
@@ -59,7 +130,49 @@ const Popup = () => {
             const messageContainer = document.createElement('div');
 
             // Render the Message component into the container
-            ReactDOM.render(<Message />, messageContainer);
+            ReactDOM.render(
+              <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[200px] justify-between"
+                  >
+                    {value
+                      ? frameworks.find((framework) => framework.value === value)?.label
+                      : "Select framework..."}
+                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0">
+                  <Command>
+                    <CommandInput placeholder="Search framework..." />
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                      {frameworks.map((framework) => (
+                        <CommandItem
+                          key={framework.value}
+                          value={framework.value}
+                          onSelect={(currentValue) => {
+                            setValue(currentValue === value ? "" : currentValue)
+                            setOpen(false)
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              value === framework.value ? "opacity-100" : "opacity-0"
+                            )}
+                          />
+                          {framework.label}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              , messageContainer);
 
             // Append the container's contents to the new element
             if (messageContainer.firstChild) {
@@ -80,9 +193,6 @@ const Popup = () => {
   }, []);
 
 
-
-
-
   return (
     <div className='h-screen w-full'>
       {state ?
@@ -96,13 +206,7 @@ const Popup = () => {
           onMouseDown={handleMouseDown}
           className='main' >
           <Navbar />
-          <HashRouter>
-            <Routes>
-              <Route path="/" element={access ? <Login /> : <Login />} />
-              <Route path="/template" element={<Template />} />
-              <Route path="/templates" element={<Templates />} />
-            </Routes>
-          </HashRouter>
+          <Login />
         </div>
         :
         <div
